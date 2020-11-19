3be5258b0e0098149f7002f58ee3773b3256f719
roadgood <dnfldnfl06@gmail.com>
2020-11-20 01:08:47 +0900

    20.11.20 fixed

--- twonum_allcase.java
+++ twonum_allcase.java
@@ -1,15 +1,17 @@
 package firstquestionJava;
-
+import java.util.Arrays;
 public class twonum_allcase {
 
 	public static void main(String[] args) {
-		
+		int[] numbers = {5,0,2,7};
+		System.out.println(Arrays.toString(Solution(numbers)));
 
 	}
 	
 	public static int[] Solution(int[] numbers) {
 		int length = numbers.length;
-		boolean[] check = new boolean[numbers[length]*2];
+		Arrays.sort(numbers);
+		boolean[] check = new boolean[2*numbers[length-1]+1];
 		int count = 0;
 		for(int i=0;i<length;i++) {
 			for(int j=0;j<length;j++) {
@@ -22,7 +24,7 @@ public class twonum_allcase {
 		}
 		int j =0;
 		int[] answer = new int[count];
-		for(int i =0;i<length;i++) {
+		for(int i =0;i<check.length;i++) {
 			if(check[i]==true) {
 				answer[j++] = i;  
 			}
