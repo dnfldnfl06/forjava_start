cd47b3bc0fe40406999af8822ed7bf9c9c4090d4
dnfldnfl06 <56574392+dnfldnfl06@users.noreply.github.com>
2020-11-23 23:58:58 +0900

    Add files via upload
    Lower level sort  double for

---
+++ doubleForSort.java
@@ -0,0 +1,10 @@
+package firstquestionJava;
+import java.util.Arrays;
+public class test44 {
+
+	public static void main(String[] args) {
+		int[] newArray = new int[3];
+		System.out.println(Arrays.toString(newArray));
+	}
+
+}
